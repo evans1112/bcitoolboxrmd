2a42c93c77e3d6946ba1f524575d3639b75b6f1e
Haocheng Zhu <122016359+evans1112@users.noreply.github.com>
2023-08-27 23:00:42 +0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'BCI Toolbox'
+author = 'Evans'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',  # 自动生成API文档
+    'sphinx.ext.viewcode',  # 添加源码链接
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'  # 使用的主题，可以选择其他主题
+html_static_path = ['_static']
