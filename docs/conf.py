e94deb4c658c0d8196a2a94291b27049362a72ff
Haocheng Zhu <122016359+evans1112@users.noreply.github.com>
2023-08-28 00:26:57 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,4 +11,4 @@ extensions = [
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_book_theme'  # 使用的主题，可以选择其他主题
-
+html_theme_path = sphinx_book_theme.get_html_theme_path()
