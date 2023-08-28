a36d6b01170af81ba5266563e7010cb04ab05de3
Haocheng Zhu <122016359+evans1112@users.noreply.github.com>
2023-08-28 11:05:44 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,13 +11,6 @@ extensions = [
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "sphinx_material"  # 将主题改为 sphinx_material
+html_theme = "stanford_theme"  # 将主题改为 sphinx_material
 
-# 在下面添加更多的主题选项，根据需要自定义配置
-html_theme_options = {
-    'color_primary': 'blue',
-    'color_accent': 'light-blue',
-    'logo_icon': '&#xe869',
-    'logo_text': 'BCI Toolbox',
-    'globaltoc_depth': 3,
-}
+
